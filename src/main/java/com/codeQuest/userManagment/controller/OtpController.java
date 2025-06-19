5ef2b6842a07feac89642854294b4e23a88e6521
Ahmed Kasim <qahmed204@gmail.com>
2025-06-19 13:59:27 +0300

    otp verify

--- src/main/java/com/codeQuest/userManagment/controller/OtpController.java
+++ src/main/java/com/codeQuest/userManagment/controller/OtpController.java
@@ -3,6 +3,7 @@ package com.codeQuest.userManagment.controller;
 import com.codeQuest.userManagment.service.OtpService;
 import com.codeQuest.userManagment.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,18 +16,15 @@ public class OtpController {
     @Autowired
     private OtpService otpService;
 
-    //check email if exits
-    @GetMapping("/exists")
-    public ResponseEntity<Boolean> checkEmailExists(@RequestParam String email) {
-        boolean exists = userService.doesEmailExist(email);
-        return ResponseEntity.ok(exists);
-    }
-    //forgetpassword Email Verify page
-    @PostMapping("/send")
-    public ResponseEntity<String> sendOtp(@RequestParam String email) {
+    @PostMapping("/verify-email")
+    public ResponseEntity<String> verifyEmailAndSendOtp(@RequestParam String email) {
+        if (!userService.doesEmailExist(email)) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Email does not exist.");
+        }
         String result = otpService.sendOtp(email);
         return ResponseEntity.ok(result);
     }
+
     //verify otp page
     @PostMapping("/verify")
     public ResponseEntity<String> verifyOtp(@RequestParam String email, @RequestParam String otp) {
